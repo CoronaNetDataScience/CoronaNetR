20f99390755457cc4fbe95d8fe723c547959dfde
Bob Kubinec <bobkubinec@gmail.com>
2022-07-15 20:29:47 -0400

    trying to finish up

--- R/get_event.R
+++ R/get_event.R
@@ -3,18 +3,37 @@
 #' @param countries A character vector of country name(s), e.g., c("Yemen", "Saudi Arabia"). "All" is used as the default.
 #' @param type A character vector of policy types, e.g., c("Lockdown", "Curfew"). "All" is used as the default. See https://www.coronanet-project.org/taxonomy.html? for a list of policy types.
 #' @param type_sub_cat A character vector of policy types, e.g., c("Self-testing", "Drive-in testing centers"). "All" is used as the default. See https://www.coronanet-project.org/taxonomy.html? for a list of policy subtypes and their related policy types.
-#' @param columns A character vector specifying the minimum set of columns of data to retrieve. Defaults to record/policy ID, dates,
+#' @param default_columns A character vector specifying the minimum set of columns of data to retrieve. Defaults to record/policy ID, dates,
 #'                policy targets, policy type and sub-type, and description
 #' @param additional_columns By default NULL. Select additional columns to include with the query.
 #' @param from A character vector for the earliest start date, e.g., "2019-12-31".
 #' @param to A character vector for the last end date, e.g., "2019-06-01".
 #' @param include_no_end_date TRUE/FALSE - whether to include policy records that do not yet have an end date.
-#'        By default set to false (this is a lot of records).
+#'        By default set to TRUE (this is a lot of records).
 #'
-#' @return A dataframe
+#' @return A dataframe with one record per COVID-19 policy
+#' @export
+#' @import httr
+#' @examples
+#' saudi_data <- get_event(countries = "Saudi Arabia", type = "All",
+#' type_sub_cat = "All", from = "2019-12-31", to = "2020-04-30")
 #'
-#' @examples get_event(countries = "All", type = "All", type_sub_cat = "All", from = "2019-12-31", to = "2027-07-01")
-
+#' # Each row is one policy record
+#' saudi_data
+#'
+#' # Use the additional_columns argument to add additional columns beyond the default set
+#' # In this case, we'll add the link column to get the URLs for underlying public sources
+#' saudi_data_links <- get_event(countries = "Saudi Arabia", type = "All",
+#'                               type_sub_cat = "All", from = "2019-12-31", to = "2020-04-30",additional_columns = "link")
+#'
+#' head(saudi_data_links$link)
+#'
+#' # look at a specific policy type
+#'
+#' saudi_data_subcat <- get_event(countries = "Saudi Arabia", type = "Lockdown",
+#'                                type_sub_cat = "All", from = "2019-12-31", to = "2020-04-30")
+#'
+#' head(saudi_data_subcat$description)
 get_event <- function(countries = "All",
                       type = "All",
                       type_sub_cat = "All",
@@ -24,11 +43,15 @@ get_event <- function(countries = "All",
                                   "date_announced",
                                   "date_start","date_end","date_end_spec",
                                   "country","init_country_level","province",
+                                  "target_init_same",'target_country',
+                                  "target_province","target_city","target_intl_org",
+                                  "target_other","target_who_what","target_who_gen",
+                                  "target_direction","compliance","enforcer",
                                   "city","type","type_sub_cat","description"),
                       additional_columns = NULL,
                       from = "2019-12-31",
                       to = "2027-07-01",
-                      include_no_end_date=FALSE) {
+                      include_no_end_date=TRUE) {
 
   # Errors/Warnings ----
 
@@ -41,17 +64,86 @@ get_event <- function(countries = "All",
 
   }
 
-  date_filter <- paste0("date_start=gte.",
-                        from,
-                        "&date_end=lte.",
-                        to)
+  if(include_no_end_date) {
+    # need to add date_start lte condition to date_end NULL condition
+    date_filter <- paste0("or=(and(date_start.gte.",
+                                             from,
+                                             ",date_end.lte.",
+                                             to,
+                                            "),",
+                                            "and(date_start.lte.",to,",date_end.is.null))")
 
-  columns <- paste0("select=",paste0(default_columns,additional_columns,collapse=","))
 
-  prod_query <- paste0(c(date_filter,columns), collapse="&")
+  } else {
 
-  cor_query <- GET(paste0("postgrest-1572524110.us-east-2.elb.amazonaws.com/public_release_allvars?",
-                          prod_query),
+    date_filter <- paste0("date_start=gte.",
+                          from,
+                          "&date_end=lte.",
+                          to)
+  }
+
+  if(type=="All") {
+
+    type <- NULL
+
+  } else {
+
+    if(length(type)>1) {
+
+      type <- paste0('type=in(',paste0(type,collapse=','),')')
+
+    } else {
+
+      type <- paste0('type=eq.',type)
+
+    }
+
+  }
+
+  if(type_sub_cat=="All") {
+
+    type_sub_cat <- NULL
+
+  } else {
+
+    if(length(type_sub_cat)>1) {
+
+      type <- paste0('type_sub_cat=in(',paste0(type_sub_cat,collapse=','),')')
+
+    } else {
+
+      type <- paste0('type_sub_cat=eq.',type_sub_cat)
+
+    }
+
+
+  }
+
+  if(countries=="All") {
+
+    countries <- NULL
+
+  } else {
+
+    if(length(countries)>1) {
+
+      countries <- paste0('country=in(',paste0(countries,collapse=','),')')
+
+    } else {
+
+      countries <- paste0('country=eq.',countries)
+
+    }
+
+  }
+
+
+  columns <- paste0("select=",paste0(c(default_columns,additional_columns),collapse=","))
+
+  prod_query <- paste0(c(columns,date_filter,type,type_sub_cat,countries), collapse="&")
+
+  cor_query <- GET(URLencode(paste0("postgrest-1572524110.us-east-2.elb.amazonaws.com/public_release_allvars?",
+                          prod_query)),
                    add_headers(Accept="text/csv"))
 
   coronanet <- content(cor_query,type="text/csv",encoding="UTF-8")
--- README.md
+++ README.md
@@ -8,16 +8,23 @@ date, this database provides the most comprehensive and granular
 documentation of such government policies in the world, capturing data
 for 18 broad policy categories alongside many other dimensions,
 including the initiator, target, and timing of a policy. This R package
-offers efficient and user-friendly access to the CoronaNet data.
+offers efficient and user-friendly access to the CoronaNet data via an
+HTTP API back-end.
+
+If you prefer to access the end point yourself, the API access point is
+postgrest-1572524110.us-east-2.elb.amazonaws.com/public_release_allvars
+and can be queried using read-only HTTP requests with the [postgrest API
+syntax](https://postgrest.org/en/stable/api.html#logical-operators).
 
 ## Latest Updates
 
-*Version 0.1.0* includes the first released function for the CoronaNetR, `get_event()`, offering users easy access to the CoronaNet Event Dataset.
+*Version 0.2.0* includes the first public beta API access using the
+`get_event()` function, offering users easy access to the CoronaNet
+Event Dataset as R data frames.
 
 ## Installation
 
-You can install the development version of CoronaNetR as
-follows:
+You can install the development version of CoronaNetR as follows:
 
 ``` r
 devtools::install_github("CoronaNetDataScience/CoronaNet")
@@ -35,6 +42,41 @@ and `to`).
 get_event(countries = "All", type = "All", type_sub_cat = "All", from = "2019-12-31", to = "2027-07-01")
 ```
 
+There are a lot of records that do not yet have end dates, so these are
+included by default. To exclude them, set the `include_no_end_date` to
+`FALSE`.
+
+By default a set of columns is included that can identify each record,
+but only includes the policy target, type and description. To change
+this default, you can pass a character vector to the `default_columns`
+argument. However, it is better to leave that argument unchanged and add
+any additional desired columns as a character vector to the
+`additional_columns` argument. A full list of columns is available in
+[our
+codebook](https://www.coronanet-project.org/assets/CoronaNet_Codebook_Panel.pdf).
+
 # Data Filtering Options
 
 ## Policy Type (`type`) and Subtype Options (`type_sub_cat`)
+
+The CoronaNet dataset has 20 main policy types and 258 policy sub-types
+(not all sub types are available for all main types). By default, the
+`get_events()` function will select all policy types and sub-types, but
+more specific criteria can be set by passing a character vector of
+relevant types/sub-types to the `type` and `type_sub_cat` arguments. To
+see a list of available policy types, please see [our
+codebook](https://www.coronanet-project.org/assets/CoronaNet_Codebook_Panel.pdf).
+
+## Date
+
+You can select a subset of the records using the `from` and `to`
+arguments (both must be specified) in YYYY-MM-DD format as a character
+value. These arguments correspond to the coded policy begin and end
+dates for a given policy record.
+
+## Country
+
+You can select a specific country to access records using the `country`
+argument. This will also pull sub-national policies for that country. A
+sub-national record will have a value in either the `province` or `city`
+column of the record.
